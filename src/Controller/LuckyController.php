d7d96c6f7cb9a2e61b0e7c22d081f6ab2b95fd61
Kévin MAZE <kekeproject@hotmail.fr>
2024-06-02 11:10:53 +0200

    start prohect

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController {
+
+    #[Route('lucky')]
+    public function number() : Response
+    {
+        return $this->render("lucky/number.html.twig",
+        [
+            'number' => random_int(0, 100),
+        ]);
+    }
+}
